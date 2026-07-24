a739f5ce9de1290c810cc350126fb76b6aa7851c
Sushant Kumar <sushant.kum96@gmail.com>
2026-07-24 16:35:59 +0000

    revert(anim): remove the scroll-velocity skew effect
    Per user feedback the skew wasn't wanted. Drops useScrollSkew and its use in App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useRef } from 'react';
-
 import { CursorGlow } from './components/CursorGlow/CursorGlow';
 import { Nav } from './components/Nav/Nav';
 import { Preloader } from './components/Preloader/Preloader';
 import { ScrollProgress } from './components/ScrollProgress/ScrollProgress';
 import { SkipLink } from './components/SkipLink/SkipLink';
-import { useScrollSkew } from './hooks/useScrollSkew';
 import { About } from './sections/About/About';
 import { Contact } from './sections/Contact/Contact';
 import { Experience } from './sections/Experience/Experience';
@@ -13,27 +10,22 @@ import { Hero } from './sections/Hero/Hero';
 import { Projects } from './sections/Projects/Projects';
 import { Skills } from './sections/Skills/Skills';
 
-const App = () => {
-  const mainRef = useRef<HTMLElement>(null);
-  useScrollSkew(mainRef);
-
-  return (
-    <>
-      <Preloader />
-      <CursorGlow />
-      <ScrollProgress />
-      <SkipLink />
-      <Nav />
-      <main id="main" ref={mainRef}>
-        <Hero />
-        <About />
-        <Experience />
-        <Projects />
-        <Skills />
-      </main>
-      <Contact />
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Preloader />
+    <CursorGlow />
+    <ScrollProgress />
+    <SkipLink />
+    <Nav />
+    <main id="main">
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Skills />
+    </main>
+    <Contact />
+  </>
+);
 
 export default App;
